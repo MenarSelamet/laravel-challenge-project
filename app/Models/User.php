c3cb3f089c366475178d49cbfb56fc009134e2ee
Menar Selamet <menarselamet007@gmail.com>
2024-08-21 20:05:06 +0300

    Creating a relation between user and integration

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function integration()
+    {
+        return $this->hasMany(Integration::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
